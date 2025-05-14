e6cb096786b99529d1197e1ea24fb79814ac1b38
gallagsu <suegallagher@gmail.com>
2025-05-14 17:11:33 +0100

    country picker default value fix

--- src/CountryYearPicker.js
+++ src/CountryYearPicker.js
@@ -29,7 +29,10 @@ function CountryYearPicker({ country, setCountry, year, setYear, error, setError
   const years = [
     { label: "2025", value: "2025" },
     { label: "2024", value: "2024" },
-    { label: "2023", value: "2023" }
+    { label: "2023", value: "2023" },
+    { label: "2022", value: "2022" },
+    { label: "2021", value: "2021" },
+    { label: "2020", value: "2020" },
   ];
 
   const collection2 = useMemo(
@@ -101,7 +104,7 @@ function CountryYearPicker({ country, setCountry, year, setYear, error, setError
           <Select.Control>
             <Select.Trigger>
             <Select.ValueText>
-                {years.find(c => c.value === year)?.label || "Choose..."}
+                {years.find(y => y.value === year)?.label || "Choose..."}
               </Select.ValueText>
             </Select.Trigger>
             <Select.IndicatorGroup>
